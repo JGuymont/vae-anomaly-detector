a94970200959a8957c7364a42d4739c979d8f6c3
Jonathan Guymont <j.guymont@gmail.com>
2018-11-06 09:06:11 -0500

    tex update

--- core/data.py
+++ core/data.py
@@ -1,122 +1,158 @@
-import unicodedata
-import string
-from io import open
+import random
+import numpy as np
 import pandas as pd
+
+import torch
+from torch.autograd import Variable
 
 from sklearn.feature_extraction.text import CountVectorizer
 
-class Data:
-    PONCTUATION = [',', '.', ';', ':', '?', '!']
+class Data(CountVectorizer):
+    """Abstract class for the smsSpamCollection
 
-    def __init__(self, path, ponctuation=PONCTUATION, **kargs):
-        self._ponctuation = ponctuation
-        self._data = self._read_data(path)
-        self.preprocessed_data = self._preprocess_data()
+    Args
+        path: (string) path to the dataset
+        split: (list) list of float [train_pct, valid_pct, test_pct]
+    """
+
+    PUNCTUATION = [',', '.', ';', ':', '?', '!']
+    LABELS = {'spam': 1, 'ham': 0}
+
+    def __init__(self, path, split, labels=LABELS, punctuation=PUNCTUATION, **kargs):
+        super().__init__(self, binary=True, **kargs)
+        self.labels = labels
+        self._punctuation = punctuation
+        self._dataframe = pd.read_table(path, delimiter=',')
+        self._data = self._read_data()
+        self._data_index = self._split_index(split)
 
     def __len__(self):
-        return len(self._inputs)
+        return len(self._data)
+
+    def __getitem__(self, i):
+        return self._data[i]
+
+    def _read_data(self):
+        sms = self._dataframe.message
+        targets = self._dataframe.target
+        return list(zip(sms, targets))
+
+    def _split_index(self, split):
+        storage = {'train': [], 'valid': [], 'test': []}
+        train_size = round(len(self)*split[0])
+        valid_size = round((len(self) - train_size)*split[1])
+
+        examples = range(len(self))
+        storage['train'] = random.sample(examples, train_size)
+        examples = [ex for ex in examples if ex not in storage['train']] # remove index
+        storage['valid'] = random.sample(examples, valid_size)
+        storage['test'] = [ex for ex in examples if ex not in storage['valid']]
+        return storage
+
+    def _relabel(self, targets):
+        for label in self.labels.keys():
+            targets[targets==label] = self.labels[label]
+        return targets
+
+    def _remove_punctuation(self, sms):
+        for punc in self._punctuation:
+            sms = sms.replace(punc, ' ')
+        return sms
+
+    def _preprocess(self, x):
+        x = x.lower()
+        x = self._remove_punctuation(x)
+        return x
+
+    def preprocess(self):
+        sms = self._dataframe.message
+        sms = [self._preprocess(s) for s in sms]
+        targets = self._relabel(self._dataframe.target)
+        self._data = list(zip(sms, targets))
+
+    def tokenize(self):
+        for i in range(len(self)):
+            input, target = self[i]
+            self._data[i] = (input.split(), target)
     
-    def __getitem__(self):
+    def _get_raw_document(self):
+        raw_document = []
+        for i in self._data_index['train']:
+            input, _ = self[i]
+            raw_document.append(input)
+        return raw_document
 
-    def _read_data(self, path):
-        with open(path) as f:
-            lines = [line for line in f]
-        data = []
-        for line in lines:
-            y, x = self._preprocess_line(line)
-            targets.append(y)
-            inputs.append(x)    
-        return inputs, targets
-        return data[1:]
+    def vectorize(self):
+        """Count vectorization
 
-    def _extract_target(self, line):
-        if line[0:3] == 'ham':
-            target = 0
-            line = line[4:]
-        elif line[0:4] == 'spam':
-            target = 1
-            line = line[5:]
-        else:
-            exit('line do not have a valid target')
-        return target, line
+        *) Word of one letter are removed*
+        *) Apostrophe are removed
+        """
+        raw_document = self._get_raw_document()
+        self.fit(raw_document)
+        for i in range(len(self)):
+            input, target = self[i]
+            self._data[i] = (self.transform([input]).toarray(), target)
 
-    def _preprocess_line(self, line):
-        target, line = self._extract_target(line)
-        #line = self._unicode_to_ascii(line)
-        #line = self._remove_ponctuation(line)
-        #line = line.lower()
-        return target, line
+    def train(self):
+        return [self._data[i] for i in self._data_index['train']]
 
-    def _preprocess_data(self): 
-        inputs = []
-        targets = []
-        for line in self._data:
-            y, x = self._preprocess_line(line)
-            targets.append(y)
-            inputs.append(x)    
-        return inputs, targets
+    def valid(self):
+        return [self._data[i] for i in self._data_index['valid']]
 
-class DataLoader(CountVectorizer):
-    
-    PONCTUATION = [',', '.', ';', ':', '?', '!']
-
-    def __init__(self, path, ponctuation=PONCTUATION, **kargs):
-        self._ponctuation = ponctuation
-        self._data = self._read_data(path)
-        self.preprocessed_data = self._preprocess_data()
-    
-        super().__init__(self, **kargs)
-
-        self.vectorized_data = self.fit_transform(self.preprocessed_data[0])
-
-    def _read_data(self, path):
-        with open(path) as f:
-            data = [line for line in f]
-        return data[1:]
-    
-    
-    
-    def _remove_ponctuation(self, line):
-        for i in self._ponctuation:
-            line = line.replace(i, " ")
-        return line
-
-    def _unicode_to_ascii(self, line):
-        # Turn a Unicode string to plain ASCII.
-        # thanks to http://stackoverflow.com/a/518232/2809427
-        
-        ALL_LETTERS = string.ascii_letters + " .,;'"
-        
-        return ''.join(
-            c for c in unicodedata.normalize('NFD', line)
-            if unicodedata.category(c) != 'Mn'
-            and c in ALL_LETTERS
-        )
-
-    def _preprocess_line(self, line):
-        target, line = self._extract_target(line)
-        #line = self._unicode_to_ascii(line)
-        #line = self._remove_ponctuation(line)
-        #line = line.lower()
-        return target, line
-    
-    def _preprocess_data(self):
-        inputs = []
-        targets = []
-        for line in self._data:
-            y, x = self._preprocess_line(line)
-            targets.append(y)
-            inputs.append(x)    
-        return inputs, targets
+    def test(self):
+        return [self._data[i] for i in self._data_index['test']]
 
     def bow_to_string(self, bow):
+        """does not work"""
         string = ''
         for i in bow:
             curr_word = self.vocabulary_[i]
             string += '{} '.format(curr_word)
         return string
 
+    def input_dim_(self):
+        return len(self.vocabulary_)
 
+class DataLoader:
 
-    
-    
+    def __init__(self, data, batch_size):
+        self._data = data
+        self._inputs, self._targets = [list(t) for t in zip(*data)] 
+        self._batch_size = batch_size
+        self._data_size = len(self._data)
+        self._n_batch = self._data_size // self._batch_size + 1
+
+        self._dataloader = self._split_in_batch()
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, i):
+        return self._dataloader[i]
+
+    def _stack_inputs(self, inputs):
+        pass
+
+    def _get_next_input_batch(self, last=False):
+        last_idx = self._batch_size if not last else len(self._inputs)
+        cur_batch = np.array([self._inputs.pop(0)[0] for _ in range(last_idx)])
+        return cur_batch
+
+    def _get_next_target_batch(self, last=False):
+        last_idx = self._batch_size if not last else len(self._targets)
+        cur_batch = np.array([self._targets.pop(0) for _ in range(last_idx)])
+        return cur_batch
+
+    def _split_in_batch(self):
+        storage = {'inputs': [], 'targets': []}
+        for i in range(self._n_batch-1):
+            cur_batch_inputs = Variable(torch.Tensor(self._get_next_input_batch()))
+            cur_batch_targets = Variable(torch.LongTensor(self._get_next_target_batch()))
+            storage['inputs'].append(cur_batch_inputs)
+            storage['targets'].append(cur_batch_targets)
+        cur_batch_inputs = Variable(torch.Tensor(self._get_next_input_batch(last=True)))
+        cur_batch_targets = Variable(torch.LongTensor(self._get_next_target_batch(last=True)))
+        storage['inputs'].append(cur_batch_inputs)
+        storage['targets'].append(cur_batch_targets) 
+        return list(zip(storage['inputs'], storage['targets']))
