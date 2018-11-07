4fec1cc0080caf4eae554d738047a70935cf0616
Jonathan Guymont <j.guymont@gmail.com>
2018-11-07 12:23:40 -0500

    add some relevant papers

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 import json
 import os
 import pandas as pd
+from unidecode import unidecode
 
-from core.data import read_csv
+import torch
+from torch.autograd import Variable
+
+from core.data import Data, DataLoader
+from core.vae import VAE
 
 CONFIGS = json.load(open('config.json', 'r'))
-
+use_cuda = torch.cuda.is_available()
+device = torch.device("cuda" if use_cuda else "cpu")
+#device = 'cpu'
 def main():
     data_path = os.path.join(CONFIGS['data']['dir'], CONFIGS['data']['filename'])
-    inputs, targets = read_csv(data_path)
-    print(inputs)
+    data = Data(data_path, split=[0.7, 0, 0.3])
+    data.preprocess()
+    data.vectorize()
+
+    trainloader = DataLoader(data.train(), batch_size=128) 
+
+    vae = VAE(data.input_dim_(), 256, 128, device).to(device)
+
+    vae.train(trainloader)
+    
 if __name__ == '__main__':
     main()
