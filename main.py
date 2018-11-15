632e787b5b3810a921f45f808f5ce620c358fe63
Jonathan Guymont <j.guymont@gmail.com>
2018-11-15 08:42:47 -0500

    train model

--- main.py
+++ main.py
@@ -13,17 +13,20 @@ CONFIGS = json.load(open('config.json', 'r'))
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 #device = 'cpu'
+
 def main():
     data_path = os.path.join(CONFIGS['data']['dir'], CONFIGS['data']['filename'])
-    data = Data(data_path, split=[0.7, 0, 0.3])
+    data = Data(data_path, split=[0.7, 0.15, 0.15])
     data.preprocess()
     data.vectorize()
 
-    trainloader = DataLoader(data.train(), batch_size=128) 
+    trainloader = DataLoader(data.train(), batch_size=128)
+    trainloader2 = DataLoader(data.train(), batch_size=1)
+    devloader = DataLoader(data.valid(), batch_size=1)
 
     vae = VAE(data.input_dim_(), 256, 128, device).to(device)
 
-    vae.train(trainloader)
+    vae.train(trainloader, trainloader2, devloader)
     
 if __name__ == '__main__':
     main()
