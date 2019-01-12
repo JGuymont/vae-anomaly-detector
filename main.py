fc0dec3c4546a997b0019cf2bd0f2cb03e3a6cf7
Jonathan Guymont <j.guymont@gmail.com>
2019-01-12 12:48:40 -0500

    finalize experiment

--- main.py
+++ main.py
@@ -1,32 +1,100 @@
+"""
+Main experiment
+"""
 import json
-import os
-import pandas as pd
-from unidecode import unidecode
-
-import torch
-from torch.autograd import Variable
-
-from core.data import Data, DataLoader
+import argparse
+from core.data import Data
 from core.vae import VAE
+from core import utils
+from core.data import DataLoader
 
-CONFIGS = json.load(open('config.json', 'r'))
-use_cuda = torch.cuda.is_available()
-device = torch.device("cuda" if use_cuda else "cpu")
-#device = 'cpu'
+MODELS = ['bow', 'binary_bow', 'boc']
 
-def main():
-    data_path = os.path.join(CONFIGS['data']['dir'], CONFIGS['data']['filename'])
-    data = Data(data_path, split=[0.7, 0.15, 0.15])
-    data.preprocess()
-    data.vectorize()
+def argparser():
+    """
+    Command line argument parser
+    """
+    parser = argparse.ArgumentParser(description='VAE spam detector')
 
-    trainloader = DataLoader(data.train(), batch_size=128)
-    trainloader2 = DataLoader(data.train(), batch_size=1)
-    devloader = DataLoader(data.valid(), batch_size=1)
+    parser.add_argument('--model', '-m', type=str, choices=['bow', 'binary_bow', 'boc', 'all'])
+    parser.add_argument('--config', '-c', type=str, default='01')
+    parser.add_argument('--n_epochs', '-e', type=int, default=None)
+    parser.add_argument('--restore', type=int, default=None)
+    parser.add_argument('--save', type=bool, default=True)
+    parser.add_argument('--device', type=str, default='cuda', choices=['cpu', 'cuda'])
+    parser.add_argument('--task', '-t', type=str, default='train', choices=['train', 'eval', 'plot'])
+    parser.add_argument('--lr', type=float, default=None)
 
-    vae = VAE(data.input_dim_(), 256, 128, device).to(device)
+    return parser.parse_args()
 
-    vae.train(trainloader, trainloader2, devloader)
-    
+def load_config(args):
+    """
+    Load json configuration file
+    """
+    path = './configs/{}/config{}.json'.format(args.model, args.config)
+    config = json.load(open(path, 'r'))
+    if args.n_epochs is not None:
+        config['training']['n_epochs'] = args.n_epochs
+    if args.lr is not None:
+        config['training']['lr'] = args.lr
+    return config
+
+def main(args):
+    """
+    Lunch the expriment
+    """
+    if args.task == 'train':
+        if args.model == 'all':
+            for model in MODELS:
+                args.model = model
+                config = load_config(args)
+                data = Data(config['data'])
+                data.vectorize()
+                vae = VAE(data.input_dim_, config, args.device).to(args.device)
+                if args.restore is not None:
+                    vae.restore_model(args.restore)
+                vae.fit(data.train, cur_epoch=args.restore)
+            return
+        config = load_config(args)
+        data = Data(config['data'])
+        data.vectorize()
+        print(data.data_size_)
+        exit()
+        vae = VAE(data.input_dim_, config, args.device).to(args.device)
+        if args.restore is not None:
+            vae.restore_model(args.restore)
+        vae.fit(data.train, cur_epoch=args.restore)
+    if args.task == 'plot':
+        models = ['{}{}'.format(model, args.config) for model in ['bow', 'binarybow', 'boc']]
+        precision, recall, logp, kldiv, log_densities, params = utils.load_results(args.config)
+        utils.plot_precision(precision, models, args.config)
+        utils.plot_recall(recall, models, args.config)
+        utils.plot_logp(logp, models, args.config)
+        utils.plot_kldiv(kldiv, models, args.config)
+        for model in models:
+            utils.hist_densities(log_densities[model], model)
+            utils.hist_param(params[model].reshape(-1), model)
+    if args.task == 'eval':
+        config = load_config(args)
+        data = Data(config['data'])
+        data.vectorize()
+        vae = VAE(data.input_dim_, config, args.device).to(args.device)
+        if args.restore is not None:
+            vae.restore_model(args.restore)
+        vae.eval()
+        testloader = DataLoader(data.test, batch_size=len(data.test))
+        precisions, recalls, logliks, kldivs = [], [], [], []
+        for _ in range(100):
+            _, _, precision, recall = vae.evaluate(testloader)
+            loglik, kldiv = vae.evaluate_loss(testloader)
+            precisions.append(precision)
+            recalls.append(recall)
+            logliks.append(loglik)
+            kldivs.append(kldiv)
+        print(utils.mean_confidence_interval(precisions))
+        print(utils.mean_confidence_interval(recalls))
+        print(utils.mean_confidence_interval(logliks))
+        print(utils.mean_confidence_interval(kldivs))
+
 if __name__ == '__main__':
-    main()
+    main(argparser())
