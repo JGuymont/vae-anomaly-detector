f13648d06647a24ac89d787fcc2920a9c9b52e9e
Jonathan Guymont <j.guymont@gmail.com>
2019-01-12 12:53:10 -0500

    add comments

--- core/vae.py
+++ core/vae.py
@@ -1,46 +1,56 @@
+#!/usr/bin/python3
+"""
+Pytorch Variational Autoendoder Network Implementation
+"""
 from itertools import chain
-import numpy as np
-
 import time
+import pickle
+import numpy as np
 import torch
 from torch.autograd import Variable
 from torch import nn
-
-from sklearn.metrics import f1_score
+from torch import optim
+from sklearn.metrics import f1_score, accuracy_score, precision_score, recall_score
+from core.data import DataLoader
 
 
 class Encoder(nn.Module):
-    """Probabilistic Encoder
+    """
+    Probabilistic Encoder
 
     Return the mean and the variance of z ~ q(z|x). The prior
-    of x is assume to be normal(0, I). 
-    
-    Args
-        x_dim:
-        h_dim:
-        z_dim:
+    of x is assume to be normal(0, I).
 
-    Return: (mu_z, log_var_z)
+    Arguments:
+        input_dim {int} -- number of features
+
+    Returns:
+        (tensor, tensor) -- mean and variance of the latent variable
+            output from the forward propagation
     """
-
-    def __init__(self, x_dim, h_dim, z_dim):
+    def __init__(self, input_dim, config):
         super(Encoder, self).__init__()
-        self._main_network = nn.Sequential(
-            nn.Linear(x_dim, h_dim),    
-            nn.ReLU()
-        )
-        self._mu = nn.Linear(h_dim, z_dim)
-        self._log_var = nn.Linear(h_dim, z_dim)
-        self._initialize_parameters()
+        config['encoder'][0]['in_features'] = input_dim
+        encoder_network = []
+        for layer in config['encoder']:
+            if layer['type'] == 'linear':
+                encoder_network.append(nn.Linear(layer['in_features'], layer['out_features']))
+            elif layer['type'] == 'relu':
+                encoder_network.append(nn.ReLU())
+            elif layer['type'] == 'tanh':
+                encoder_network.append(nn.Tanh())
+            elif layer['type'] == 'dropout':
+                encoder_network.append(nn.Dropout(layer['rate']))
+            elif layer['type'] == 'batch_norm':
+                encoder_network.append(nn.BatchNorm1d(layer['num_features']))
+        self.encoder_network = nn.Sequential(*encoder_network)
+        self.read_mu = nn.Linear(config['read_mu']['in_features'], config['latent_dim'])
+        self.read_logvar = nn.Linear(config['read_sigma']['in_features'], config['latent_dim'])
+        self.initialize_parameters()
 
-    def _initialize_parameters(self):
-        """Parameter initialization
-
-            W ~ U[-1 / nc, 1 / nc]
-        
-        where nc is the number of neuros on the 
-        previous layer. The bias are initialize at
-        0.
+    def initialize_parameters(self):
+        """
+        Xavier initialization
         """
         for layer in self.modules():
             if isinstance(layer, nn.Linear):
@@ -49,187 +59,286 @@ class Encoder(nn.Module):
                 layer.bias.data.zero_()
 
     def forward(self, inputs):
-        """Forward propagation"""
-        hidden_state = self._main_network(inputs)
-        mu = self._mu(hidden_state)
-        log_var = self._log_var(hidden_state) 
-        return mu, log_var
-
+        """
+        Forward propagation
+        """
+        hidden_state = self.encoder_network(inputs)
+        mean = self.read_mu(hidden_state)
+        logvar = self.read_logvar(hidden_state) 
+        return mean, logvar
 
 class Decoder(nn.Module):
-    """Decoder Decoder
     """
-    def __init__(self, x_dim, h_dim, z_dim):
+    Decoder
+    """
+    def __init__(self, input_dim, config):
         super(Decoder, self).__init__()
-        self._decoder = nn.Sequential(
-            nn.Linear(z_dim, h_dim),
-            nn.ReLU(),
-            nn.Linear(h_dim, x_dim),
-            nn.Sigmoid()
-        )
-        self._initialize_parameters()
+        self._distr = config['distribution']
 
-    def _initialize_parameters(self):
+        decoder_network = []
+        for layer in config['decoder']:
+            if layer['type'] == 'linear':
+                decoder_network.append(nn.Linear(layer['in_features'], layer['out_features']))
+            elif layer['type'] == 'relu':
+                decoder_network.append(nn.ReLU())
+            elif layer['type'] == 'relu6':
+                decoder_network.append(nn.ReLU6())
+            elif layer['type'] == 'tanh':
+                decoder_network.append(nn.Tanh())
+            elif layer['type'] == 'sigmoid':
+                decoder_network.append(nn.Sigmoid())
+            elif layer['type'] == 'dropout':
+                decoder_network.append(nn.Dropout(layer['rate']))
+            elif layer['type'] == 'batch_norm':
+                decoder_network.append(nn.BatchNorm1d(layer['num_features']))
+            elif layer['type'] == 'read_x':
+                decoder_network.append(nn.Linear(layer['in_features'], input_dim))
+        self.decoder = nn.Sequential(*decoder_network)
+        if self._distr == 'poisson':
+            self.read_alpha = nn.Sequential(
+                nn.Linear(config['latent_dim'], input_dim),
+                nn.ReLU6()
+            )
+        self.initialize_parameters()
+
+    def initialize_parameters(self):
         for layer in self.modules():
             if isinstance(layer, nn.Linear):
-                nc = layer.in_features
-                stdv = 1/np.sqrt(nc)
-                layer.weight.data.uniform_(-stdv, stdv)
+                bound = 1/np.sqrt(layer.in_features)
+                layer.weight.data.uniform_(-bound, bound)
                 layer.bias.data.zero_()
 
     def forward(self, z):
-        x = self._decoder(z)
-        return x
-
+        if self._distr == 'poisson':
+            alpha = 0.5 * self.read_alpha(z)
+            return alpha * self.decoder(z)
+        else:
+            return self.decoder(z)
 
 class VAE(nn.Module):
     """
     VAE, x --> mu, log_sigma_sq --> N(mu, log_sigma_sq) --> z --> x
     """
-
-    _EPS = 1e-10
-
-    def __init__(self, x_dim, h_dim, z_dim, device, eps=_EPS):
+    def __init__(self, input_dim, config, device):
         super(VAE, self).__init__()
-        self._eps = eps
+        self.config = config
+        self._model_name = config['name']
+        self._distr = config['model']['distribution']
         self._device = device
-        self._encoder = Encoder(x_dim, h_dim, z_dim)
-        self._decoder = Decoder(x_dim, h_dim, z_dim)
-        self._optim = torch.optim.Adam(
-            self.parameters(), 
-            lr=1.e-3, 
-            betas=(0.5, 0.999)
-        )
-        optimizer = torch.optim.SGD(self.parameters(), lr=0.05, momentum=0.1)
-        self._bce_loss = torch.nn.BCELoss(reduction='sum')
-        self._num_epochs = 100
-        
-        self.mu = None 
-        self.log_var = None
+        self._encoder = Encoder(input_dim, config['model'])
+        self._decoder = Decoder(input_dim, config['model'])
 
-        self.precentile_threshold = 10
+        self.batch_size = config['training']['batch_size']
+        self.num_epochs = config['training']['n_epochs']
+
+        self._optim = optim.Adam(self.parameters(), lr=config['training']['lr'], betas=config['training']['betas'])
+
+        self.mu = None
+        self.logvar = None
+
+        self.precentile_threshold = config['model']['threshold']
         self.threshold = None
 
     def parameters(self):
         return chain(self._encoder.parameters(), self._decoder.parameters())
 
-    def _sample_z(self, mu, log_var):
+    def _sample_z(self, mu, logvar):
         epsilon = torch.randn(mu.size())
         epsilon = Variable(epsilon, requires_grad=False).type(torch.FloatTensor).to(self._device)
-        sigma = torch.exp(log_var / 2)
+        sigma = torch.exp(logvar / 2)
         return mu + sigma * epsilon
 
     def forward(self, inputs):
-        self.mu, self.log_var = self._encoder(inputs)
-        z = self._sample_z(self.mu, self.log_var)
-        x = self._decoder(z)
-        return x
+        """
+        Forward propagation
+        """
+        self.mu, self.logvar = self._encoder(inputs)
+        latent = self._sample_z(self.mu, self.logvar)
+        theta = self._decoder(latent)
+        return theta
 
-    def to_numpy(self, tensor):
+    def _to_numpy(self, tensor):
         return tensor.data.cpu().numpy()
 
-    def train(self, trainloader, trainloader2=None,  devloader=None, print_every=10):
+    def loglikelihood(self, reduction):
+        """
+        Return the log-likelihood
+        """
+        if self._distr == 'poisson':
+            return nn.PoissonNLLLoss(reduction=reduction)
+        elif self._distr == 'bernoulli':
+            return nn.BCELoss(reduction=reduction)
+        else: 
+            raise ValueError('{} is not a valid distribution'.format(self._distr))
 
-        to_np = lambda x: x.data.cpu().numpy()
 
-        loss_storage = {'train': [], 'valid': []}
-        acc_storage = {'train': [], 'valid': []}
+    def fit(self, data, cur_epoch=None, print_every=1):
+        """
+        Train the neural network
+        """
+        self.cur_epoch = 0 if cur_epoch is None else cur_epoch
 
-        starting_time = time.time()
+        start_time = time.time()
 
-        for epoch in range(self._num_epochs):
-            #if epoch >= 1:
-            #    print("\n[%2.2f]" % (time.time() - t0), end="\n")
-            
-            t0 = time.time()
+        storage = {
+            'loss': [], 'kldiv': [], '-logp(x|z)': [],
+            'precision': [], 'recall': [], 'log_densities': None, 'params': None
+        }
 
-            for step, (inputs, _) in enumerate(trainloader, 0):
-                #if step >= 1:
-                #    print("Epoch [%i] | iter [%i] | time [%2.2f] | loss [%2.2f]" % (
-                #        epoch, step, time.time() - t1, to_np(loss)), end="\r"
-                #    )
-                t1 = time.time()
+        dataloader = DataLoader(data, batch_size=self.batch_size)
 
-                batch_size = inputs.size(0)
+        for epoch in range(self.cur_epoch, self.cur_epoch+self.num_epochs):
 
-                x = inputs.to(self._device)
-                gamma = self.forward(x)
+            self.cur_epoch += 1
 
-                log_likelihood = self._bce_loss(gamma+self._eps, x) / batch_size
-                kl_divirgence = torch.mean(.5 * torch.sum(self.mu**2 + torch.exp(self.log_var) - 1 - self.log_var, 1))
-                loss = log_likelihood + kl_divirgence
-                
+            # temporary storage
+            losses, kldivs, neglogliks = [], [], []
+
+            for inputs, _ in dataloader:
+                inputs = inputs.to(self._device)
+                logtheta = self.forward(inputs)
+                loglikelihood = - self.loglikelihood(reduction='sum')(logtheta, inputs) / inputs.shape[0]
+                kl_div = -0.5 * torch.sum(1 + self.logvar - self.mu.pow(2) - self.logvar.exp()) / inputs.shape[0]
+                loss = -loglikelihood + kl_div
                 loss.backward()
-
                 self._optim.step()
-                self._optim.zero_grad() 
-            
-            if epoch % (self._num_epochs // print_every) == 0 and epoch > 0:
-                self.find_threshold(trainloader2)
-                current_time = time.time()
-                loss = self.evaluate_loss(trainloader)
-                train_acc = self.eval_f1_score(trainloader2)
-                dev_acc = self.eval_f1_score(devloader)
-                print('epoch: {} | loss: {} | train F1: {} | valid F1: {}% | time: {}'.format(
-                    epoch, loss, train_acc, dev_acc, self._get_time(starting_time, current_time)
-                ))
-    
+                self._optim.zero_grad()
+                losses.append(self._to_numpy(loss))
+                kldivs.append(self._to_numpy(kl_div))
+                neglogliks.append(self._to_numpy(-loglikelihood))
+
+            storage['loss'].append(np.mean(losses))
+            storage['kldiv'].append(np.mean(kldivs))
+            storage['-logp(x|z)'].append(np.mean(neglogliks))
+
+            if (epoch+1) % print_every == 0:
+                epoch_time = self._get_time(start_time, time.time())
+                self.eval()
+                f1, acc, prec, recall = self.evaluate(dataloader)
+                storage['precision'].append(prec)
+                storage['recall'].append(recall)
+                print('epoch: {} | loss: {:.3f} | -logp(x|z): {:.3f} | kldiv: {:.3f} | time: {}'.format(
+                    epoch+1,
+                    storage['loss'][-1],
+                    storage['-logp(x|z)'][-1],
+                    storage['kldiv'][-1],
+                    epoch_time))
+                print('F1. {:.3f} | acc. {:.3f} | prec.: {:.3f} | rec. {:.3f}'.format(f1, acc, prec, recall))
+                self.train() # Put model back in train mode
+
+        storage['log_densities'] = self._get_densities(dataloader)
+        storage['params'] = self._get_parameters(dataloader)
+        with open('./results/{}.pkl'.format(self._model_name), 'wb') as _f:
+            pickle.dump(storage, _f, pickle.HIGHEST_PROTOCOL)
+
+        self.save()
+
+    def _remove_spam(self, dataloader, data):
+        idx_to_remove = self._find_threshold(dataloader)
+        data.pop(idx_to_remove)
+        self._encoder.initialize_parameters()
+        self._decoder.initialize_parameters()
+        self._optim = optim.Adam(self.parameters(), lr=self.lr, betas=(0.5, 0.999))
+        return data
+
+
     def _get_time(self, starting_time, current_time):
         total_time = current_time - starting_time
         minutes = round(total_time // 60)
         seconds = round(total_time % 60)
         return '{} min., {} sec.'.format(minutes, seconds)
 
-    def evaluate_loss(self, dataloader):
-        loss = 0
+    def _get_parameters(self, dataloader):
+        self.eval()
+        parameters = []
         for inputs, _ in dataloader:
-            x = inputs.to(self._device)
-            gamma = self.forward(x)
-            loss += self._bce_loss(gamma, x)
-
-        return round(self.to_numpy(loss) / dataloader.data_size_(), 4)
-
-    def evaluate_probability(self, x):
-        x = x.to(self._device)
-        gamma = self.forward(x)
-        log_likelihood = - self._bce_loss(gamma, x)
-        return self.to_numpy(log_likelihood)
-
-    def find_threshold(self, dataloader):
-        log_densities = []
-        for input_, _ in dataloader:
-            log_density = self.evaluate_probability(input_)
-            log_densities.append(log_density)
-        log_densities = np.array(log_densities)
-        self.threshold = np.percentile(log_densities, self.precentile_threshold)
-
-    def predict(self, x):
-        log_density = self.evaluate_probability(x)
-        if log_density < self.threshold:
-            return 1
+            inputs = inputs.to(self._device)
+            logtheta = self._to_numpy(self.forward(inputs))
+            parameters.extend(logtheta)
+        self.train()
+        if self._distr == 'poisson':
+            parameters = np.exp(np.array(parameters))
         else:
-            return 0
+            parameters = np.array(parameters)
+        return parameters
 
-    def eval_f1_score(self, dataloader):
+    def _evaluate_probability(self, inputs):
+        inputs = inputs.to(self._device)
+        logtheta = self.forward(inputs)
+        log_likelihood = - self.loglikelihood(reduction='none')(logtheta, inputs)
+        log_likelihood = torch.sum(log_likelihood, 1)
+        return self._to_numpy(log_likelihood)
+
+    def _get_densities(self, dataloader):
+        self.eval()
+        log_densities = []
+        for inputs, _ in dataloader:
+            log_density = self._evaluate_probability(inputs)
+            log_densities.extend(log_density)
+        self.train()
+        log_densities = np.array(log_densities)
+        return log_densities
+
+    def _find_threshold(self, dataloader):
+        log_densities = self._get_densities(dataloader)
+        lowest_density = np.argmin(log_densities)
+        self.threshold = np.percentile(log_densities, self.precentile_threshold)
+        return lowest_density
+
+    def predict(self, inputs):
+        """
+        Predict the class of the inputs
+        """
+        log_density = self._evaluate_probability(inputs)
+        predictions = np.zeros_like(log_density).astype(int)
+        predictions[log_density < self.threshold] = 1
+        return list(predictions)
+
+    def evaluate(self, dataloader):
+        """
+        Evaluate accuracy.
+        """
+        self.eval()
+        self._find_threshold(dataloader)
         predictions = []
-        targets = [] 
-        for input_, target in dataloader:
-            pred = self.predict(input_)
-            predictions.append(pred)
-            targets.append(self.to_numpy(target))
-        return round(f1_score(targets, predictions), 4)
+        ground_truth = []
 
-    def evaluate_accuracy(self, dataloader):
-        total, correct = 0, 0
-        for input_, target in dataloader:
-            pred = self.predict(input_)
-            if pred == target.data:
-                correct += 1
-            total += 1
-        return round((correct / total) * 100, 4)
-            
+        for inputs, targets in dataloader:
+            pred = self.predict(inputs)
+            predictions.extend(pred)
+            ground_truth.extend(list(self._to_numpy(targets)))
+        self.train() # Put model back in train mode
 
+        f1 = f1_score(ground_truth, predictions)
+        accuracy = accuracy_score(ground_truth, predictions)
+        precision = precision_score(ground_truth, predictions)
+        recall = recall_score(ground_truth, predictions)
 
-if __name__ == '__main__':
-    
-    pass
+        return f1, accuracy, precision, recall
+
+    def evaluate_loss(self, dataloader):
+        loglikelihood, kl_div = 0., 0. 
+        for inputs, _ in dataloader:
+            inputs = inputs.to(self._device)
+            logtheta = self.forward(inputs)
+            loglikelihood += - self._to_numpy(self.loglikelihood(reduction='sum')(logtheta, inputs)) / inputs.shape[0]
+            kl_div += -0.5 * self._to_numpy(torch.sum(1 + self.logvar - self.mu.pow(2) - self.logvar.exp())) / inputs.shape[0]
+        return loglikelihood, kl_div
+
+    def save(self):
+        """Save model paramers under config['model_path']"""
+        model_path = './{}/{}_{}.pt'.format(self.config['model']['save_dir'], self.config['name'], self.cur_epoch)
+        checkpoint = {
+            'model_state_dict': self.state_dict(),
+            'optimizer_state_dict': self._optim.state_dict()
+        }
+        torch.save(checkpoint, model_path)
+
+    def restore_model(self, epoch):
+        """
+        Retore the model parameters
+        """
+        model_path = './{}/{}_{}.pt'.format(self.config['model']['save_dir'], self.config['name'], epoch)
+        checkpoint = torch.load(model_path)
+        self.load_state_dict(checkpoint['model_state_dict'])
+        self._optim.load_state_dict(checkpoint['optimizer_state_dict'])
