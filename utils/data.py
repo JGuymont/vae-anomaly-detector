12ff6422b01d24a64c4f9ec1eb4ac7267de2ad27
unknown <j.guymont@gmail.com>
2019-02-17 20:18:48 -0500

    Add a input_dim property to the data abstract class in utils/data. The property input_dim is useful when initializing the weights of the model

--- utils/data.py
+++ utils/data.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-import random
-import os
-import numpy as np
 import pandas as pd
 import torch
 from torch.utils.data import Dataset
 from torch.autograd import Variable
-from sklearn.feature_extraction.text import CountVectorizer
-from core.preprocessor import Preprocessor
 
 
 class SpamDataset(Dataset):
@@ -18,15 +13,27 @@ class SpamDataset(Dataset):
         path: (string) path to the dataset
         split: (list) list of float [train_pct, valid_pct, test_pct]
     """
-    def __init__(self, csv_path, preprocess=None):
+    def __init__(self, csv_path, label2int=None, transform=None):
         self._data = pd.read_table(csv_path, delimiter=',', encoding='latin-1')
-        self._preprocess = preprocess
+        self._label2int = label2int
+        self._transform = transform
 
     def __len__(self):
         return len(self._data)
 
     def __getitem__(self, idx):
-        label = self._data.v1[idx]
-        sms = self._data.v2[idx]
-        input = sms if not self._preprocess else self._preprocess(sms)
-        return input, label
+        # Input processing
+        input = self._data.v2[idx]
+        input = input if not self._transform else self._transform(input)
+        # input = Variable(torch.Tensor(input))
+
+        # Target processing
+        target = self._data.v1[idx]
+        target = self._label2int[target] if self._label2int else target
+        # target = Variable(torch.LongTensor([target]))
+
+        return input, target
+
+    @property
+    def input_dim_(self):
+        return len(self[0][0])
